70f64fe8bdf6d4ffade56d4c34752b0c1c488fe8
matijamic <micunovicmatija2@gmail.com>
2022-03-21 19:01:31 +0200

    fix underline issue

--- src/components/common/heading.jsx
+++ src/components/common/heading.jsx
@@ -2,42 +2,42 @@ import React from "react"
 import { down } from "styled-breakpoints"
 import styled from "styled-components"
 
-const Heading = ({ title, subtitle, align = "center" }) => {
-  const Headline = styled.div`
-    text-align: ${align};
-    z-index: 1;
-  `
-  const Title = styled.h2`
-    position: relative;
-    &::after {
-      content: "";
-      position: absolute;
-      width: 55px;
-      height: 7px;
-      border-radius: 4px;
-      background-color: #4478db;
-      bottom: 10px;
-      left: ${align === "left" ? 0 : "50%"};
-      transform: ${align === "left" ? 0 : "translateX(-50%)"};
-      ${down("md")} {
-        bottom: -7px;
-      }
-      ${down("sm")} {
-        width: 32px;
-        border-radius: 2px;
-        height: 4px;
-      }
-    }
-  `
-  const Subtitle = styled.p`
-    font-size: 30px;
-    line-height: 41px;
+const Title = styled.h2`
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 55px;
+    height: 7px;
+    border-radius: 4px;
+    background-color: #4478db;
+    bottom: 10px;
+    left: 50%;
+    transform: translateX(-50%);
     ${down("md")} {
-      font-size: 25px;
-      line-height: 33px;
+      bottom: -7px;
     }
-  `
+    ${down("sm")} {
+      width: 32px;
+      border-radius: 2px;
+      height: 4px;
+    }
+  }
+`
+const Subtitle = styled.p`
+  font-size: 30px;
+  line-height: 41px;
+  ${down("md")} {
+    font-size: 25px;
+    line-height: 33px;
+  }
+`
+const Headline = styled.div`
+  text-align: center;
+  z-index: 1;
+`
 
+const Heading = ({ title, subtitle }) => {
   return (
     <Headline>
       <Title className="title">
